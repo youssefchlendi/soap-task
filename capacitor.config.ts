3492e6b933bfb068f38d5fb50c01798a4219a960
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-10 08:40:23 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'soap-task',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
