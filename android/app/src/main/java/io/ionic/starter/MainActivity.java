43868203b40f6ff03b19efdd3a19681e20df2310
youssef chlendi <youssef.chlendi@gmail.com>
2023-02-13 10:14:38 +0100

    register capacitor http

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,5 +1,12 @@
 package io.ionic.starter;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
+import com.getcapacitor.plugin.CapacitorHttp;
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        registerPlugin(CapacitorHttp.class);
+    }
+}
